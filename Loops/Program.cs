76c187079d62250e09b9be6d6b052d6202f8f31b
Kofi Junior Eshun <eshunkofi23@gmail.com>
2024-03-19 09:49:55 -0600

    Conditional Statements, Variables and Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Loops{
+    class Project{
+        static void Main(){
+            List<string> collection = new List<string>(){"a", "b", "c"};
+
+            for(int i = 0; i < collection.Count; i++){
+                System.Console.WriteLine(collection[i]);
+            }
+        }
+    }
+}
